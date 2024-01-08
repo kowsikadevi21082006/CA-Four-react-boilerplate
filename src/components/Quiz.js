888197b46673199ff1008389eaf533b0b34bc018
kowsikadevi21082006 <deva36rp@gamil.com>
2024-01-08 23:41:59 +0530

    done

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { QuizData } from './QuizData'
+import { QuizData } from '../QuizData'
 import QuizResult from './QuizResult';
 function Quiz() {
     const [currentQuestion,setCurrentQuestion]=useState(0);
